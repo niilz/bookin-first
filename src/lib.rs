24a43deb3fd096b203cdec876f2f12574c101199
Haberstroh <Nils.Haberstroh@computacenter.com>
2024-02-27 22:24:04 +0100

    add basic request response flow to main

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,13 @@
-struct LoginRequest {
+use std::collections::HashMap;
+
+const EGYM_LOGIN_URL: &str = "https://id.egym.com/login";
+
+pub struct LoginRequest {
     user_name: String,
     password: String,
 }
 impl LoginRequest {
-    fn new(user_name: &str, password: &str) -> Self {
+    pub fn new(user_name: &str, password: &str) -> Self {
         Self {
             user_name: user_name.to_string(),
             password: password.to_string(),
@@ -11,50 +15,84 @@ impl LoginRequest {
     }
 }
 
-struct LoginResponse {
+#[derive(Debug)]
+pub struct LoginResponse {
     session_token: String,
 }
 
 #[derive(Default, Debug)]
-struct LoginService<Client> {
-    token: Option<String>,
-    http_client: Client,
+pub struct LoginService<Client> {
+    pub token: Option<String>,
+    pub http_client: Client,
 }
 
-trait HttpClient {
-    fn do_login(&self, request: LoginRequest) -> Result<LoginResponse, ()>;
+pub trait HttpClient {
+    async fn do_login(&self, request: LoginRequest) -> Result<LoginResponse, ()>;
 }
 
 impl<Client> LoginService<Client>
 where
     Client: HttpClient,
 {
-    fn do_login(&mut self, request: LoginRequest) {
-        match self.http_client.do_login(request) {
+    pub async fn do_login(&mut self, request: LoginRequest) {
+        match self.http_client.do_login(request).await {
             Ok(res) => self.token = Some(res.session_token),
             Err(_) => eprintln!("oops"),
         }
     }
 }
 
-#[derive(Default, Debug)]
-struct HttpClientMock;
-impl HttpClient for HttpClientMock {
-    fn do_login(&self, _req: LoginRequest) -> Result<LoginResponse, ()> {
-        Ok(LoginResponse {
-            session_token: "session:12345".to_string(),
-        })
+pub struct ReqwestHttpClient {
+    pub client: reqwest::Client,
+}
+
+impl HttpClient for ReqwestHttpClient {
+    async fn do_login(&self, _request: LoginRequest) -> Result<LoginResponse, ()> {
+        let mut params = HashMap::new();
+        //params.insert("username", "TODO-ARG-USER");
+        //params.insert("password", "TODO-ARG-PWD");
+        //params.insert("clientId", "TODO-CLIENT_ID");
+        params.insert(
+            "callbackUrl",
+            "https://www.fitnessfirst.de/mein-fitnessfirst",
+        );
+        let res = self.client.post(EGYM_LOGIN_URL).form(&params).send().await;
+        match res {
+            Ok(res) => {
+                println!("Response: {res:?}");
+                Ok({
+                    LoginResponse {
+                        session_token: "Dummy".to_string(),
+                    }
+                })
+            }
+            Err(e) => {
+                eprintln!("oopsi: {e}");
+                Err(())
+            }
+        }
     }
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
-    #[test]
-    fn setup_service() {
+
+    #[derive(Default, Debug)]
+    struct HttpClientMock;
+    impl HttpClient for HttpClientMock {
+        async fn do_login(&self, _req: LoginRequest) -> Result<LoginResponse, ()> {
+            Ok(LoginResponse {
+                session_token: "session:12345".to_string(),
+            })
+        }
+    }
+
+    #[tokio::test]
+    async fn setup_service() {
         let mut login_service: LoginService<HttpClientMock> = Default::default();
         let req = LoginRequest::new("user", "password");
-        login_service.do_login(req);
+        login_service.do_login(req).await;
 
         assert!(login_service.token.is_some());
         assert_eq!("session:12345", login_service.token.unwrap());
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use fitness_api::{LoginRequest, LoginService, ReqwestHttpClient};
+
+#[tokio::main]
+async fn main() {
+    let http_client = ReqwestHttpClient {
+        client: reqwest::Client::new(),
+    };
+    let mut login_service = LoginService {
+        token: None,
+        http_client,
+    };
+    let login_request = LoginRequest::new("foo", "bar");
+    login_service.do_login(login_request).await;
+    println!("{:?}", login_service.token);
 }
