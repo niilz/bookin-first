5a61442cbc93c837f5d1d377c7b175e399314392
Haberstroh <Nils.Haberstroh@computacenter.com>
2024-02-29 11:13:56 +0100

    rename LoginRequest to EgymLoginRequest/-Response and add FitnessFirstLoginRequest/-Response

--- src/http_client.rs
+++ src/http_client.rs
@@ -1,13 +1,20 @@
 use std::{collections::HashMap, error::Error};
 
-use crate::{request::LoginRequest, response::LoginResponse};
+use crate::{request::EgymLoginRequest, response::EgymLoginResponse};
 
 const MEIN_FITNESS_FIRST_URL: &str = "https://www.fitnessfirst.de/mein-fitnessfirst";
 const EGYM_LOGIN_URL: &str = "https://id.egym.com/login";
 //const EGYM_LOGIN_URL: &str = "https://httpbin.org/post";
 
 pub trait HttpClient {
-    async fn egym_login(&self, request: LoginRequest) -> Result<LoginResponse, Box<dyn Error>>;
+    async fn egym_login(
+        &self,
+        request: EgymLoginRequest,
+    ) -> Result<EgymLoginResponse, Box<dyn Error>>;
+    async fn ff_login(
+        &self,
+        request: FitnessFirstLoginRequest,
+    ) -> Result<FitnessFirstLoginResponse, Box<dyn Error>>;
 }
 
 pub struct ReqwestHttpClient {
@@ -15,7 +22,10 @@ pub struct ReqwestHttpClient {
 }
 
 impl HttpClient for ReqwestHttpClient {
-    async fn egym_login(&self, request: LoginRequest) -> Result<LoginResponse, Box<dyn Error>> {
+    async fn egym_login(
+        &self,
+        request: EgymLoginRequest,
+    ) -> Result<EgymLoginResponse, Box<dyn Error>> {
         let mut params = HashMap::new();
         params.insert("username", request.user_name.as_str());
         params.insert("password", request.password.as_str());
@@ -29,7 +39,7 @@ impl HttpClient for ReqwestHttpClient {
                     res.text().await.expect("could not read response text")
                 );
                 Ok({
-                    LoginResponse {
+                    EgymLoginResponse {
                         session_token: "Dummy".to_string(),
                     }
                 })
--- src/request.rs
+++ src/request.rs
@@ -1,14 +1,25 @@
-pub struct LoginRequest {
+pub struct EgymLoginRequest {
     pub user_name: String,
     pub password: String,
     pub client_id: String,
 }
-impl LoginRequest {
+impl EgymLoginRequest {
     pub fn new(user_name: &str, password: &str, client_id: &str) -> Self {
         Self {
             user_name: user_name.to_string(),
             password: password.to_string(),
             client_id: client_id.to_string(),
+        }
+    }
+}
+
+pub struct FitnessFirstLoginRequest {
+    pub egym_token: String,
+}
+impl FitnessFirstLoginRequest {
+    pub fn new(egym_token: &str) -> Self {
+        Self {
+            egym_token: egym_token.to_string(),
         }
     }
 }
