014f38f382dadfadf090f5b1c5a809a709560d7d
Haberstroh <Nils.Haberstroh@computacenter.com>
2024-02-29 09:57:01 +0100

    refine error handling and add a failing test

--- src/login_service.rs
+++ src/login_service.rs
@@ -1,13 +1,19 @@
+use std::error::Error;
+
 use crate::{http_client::HttpClient, request::LoginRequest};
 
 impl<Client> LoginService<Client>
 where
     Client: HttpClient,
 {
-    pub async fn do_login(&mut self, request: LoginRequest) {
+    pub async fn do_login(&mut self, request: LoginRequest) -> Result<(), Box<dyn Error>> {
         match self.http_client.do_login(request).await {
-            Ok(res) => self.token = Some(res.session_token),
-            Err(_) => eprintln!("oops"),
+            Ok(res) => {
+                println!("Login succeeded");
+                self.token = Some(res.session_token);
+                Ok(())
+            }
+            Err(e) => Err(Box::from(format!("login failed: {e}"))),
         }
     }
 }
@@ -27,10 +33,18 @@ mod test {
     #[derive(Default, Debug)]
     struct HttpClientMock;
     impl HttpClient for HttpClientMock {
-        async fn do_login(&self, _req: LoginRequest) -> Result<LoginResponse, ()> {
+        async fn do_login(&self, _req: LoginRequest) -> Result<LoginResponse, Box<dyn Error>> {
             Ok(LoginResponse {
                 session_token: "session:12345".to_string(),
             })
+        }
+    }
+
+    #[derive(Default, Debug)]
+    struct FailingHttpClientMock;
+    impl HttpClient for FailingHttpClientMock {
+        async fn do_login(&self, _req: LoginRequest) -> Result<LoginResponse, Box<dyn Error>> {
+            Err(Box::from("Failed as planned for test"))
         }
     }
 
@@ -38,9 +52,20 @@ mod test {
     async fn setup_service() {
         let mut login_service: LoginService<HttpClientMock> = Default::default();
         let req = LoginRequest::new("user", "password", "client-id");
-        login_service.do_login(req).await;
+        let success = login_service.do_login(req).await;
 
+        assert!(success.is_ok());
         assert!(login_service.token.is_some());
         assert_eq!("session:12345", login_service.token.unwrap());
+    }
+
+    #[tokio::test]
+    async fn setup_fails() {
+        let mut login_service: LoginService<FailingHttpClientMock> = Default::default();
+        let req = LoginRequest::new("user", "password", "client-id");
+        let success = login_service.do_login(req).await;
+
+        assert!(success.is_err());
+        assert!(login_service.token.is_none());
     }
 }
