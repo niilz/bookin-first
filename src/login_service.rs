45503523521d1fd9fd40082ba82f1130b2ed6976
Haberstroh <Nils.Haberstroh@computacenter.com>
2024-03-12 17:02:59 +0100

    Read session from cookie-jar and test it

--- src/login_service.rs
+++ src/login_service.rs
@@ -1,14 +1,15 @@
-use std::error::Error;
+use std::{error::Error, sync::Arc};
 
 use crate::{
-    http_client::HttpClient,
+    http_client::{HttpClient, FITNESS_FIRST_BASE_URL},
     request::{EgymLoginRequest, FitnessFirstLoginRequest},
     response::Response,
 };
 
-impl<Client> LoginService<Client>
+impl<Client, Cookie> LoginService<Client, Cookie>
 where
     Client: HttpClient,
+    Cookie: crate::cookies::Cookie,
 {
     pub async fn do_login(&mut self, request: EgymLoginRequest) -> Result<(), Box<dyn Error>> {
         if self.token.is_none() {
@@ -23,6 +24,7 @@ where
                         return Err(Box::from(format!("Could not extract token from: {res}")));
                     }
                 }
+                Ok(_) => return Err(Box::from("Unexpected Response type for egym-login")),
                 Err(e) => return Err(Box::from(format!("login egym failed: {e}"))),
             };
         }
@@ -32,10 +34,12 @@ where
     async fn login_to_fitnes_first(&mut self) -> Result<(), Box<dyn Error>> {
         let ff_login_req = FitnessFirstLoginRequest::new(&self.token.as_ref().unwrap());
         match self.http_client.ff_login(ff_login_req).await {
-            Ok(res) => {
-                //self.session = Some(res.session_token);
-                println!("FF login succeeded");
-                println!("FF-RES: {res:?}");
+            Ok(_res) => {
+                println!("FF login succeeded. PHPSESSID-Cookie should be in Jar");
+                let session_cookie = self.cookie_jar.read_cookie(FITNESS_FIRST_BASE_URL)?;
+                // TODO: Split session-Id from Cookie key, maybe
+                println!("PHPSESSID: {session_cookie}");
+                self.session = Some(session_cookie);
                 Ok(())
             }
             Err(e) => Err(Box::from(format!("login fitness-first failed: {e}"))),
@@ -44,17 +48,19 @@ where
 }
 
 #[derive(Default, Debug)]
-pub struct LoginService<Client> {
+pub struct LoginService<Client, Cookie> {
     pub http_client: Client,
     token: Option<String>,
     session: Option<String>,
+    cookie_jar: Arc<Cookie>,
 }
-impl<Client> LoginService<Client> {
-    pub fn new(http_client: Client) -> Self {
+impl<Client, Cookie> LoginService<Client, Cookie> {
+    pub fn new(http_client: Client, cookie_jar: Arc<Cookie>) -> Self {
         Self {
             http_client,
             token: None,
             session: None,
+            cookie_jar,
         }
     }
 }
@@ -69,11 +75,11 @@ mod test {
         mock_client,
         request::{EgymLoginRequest, FitnessFirstLoginRequest},
         response::Response,
-        testutil::{egym_login_response_dummy, ff_login_response_dummy, MockCall},
+        testutil::{egym_login_response_dummy, ff_login_response_dummy, CookieMock, MockCall},
     };
 
     const EGYM_JWT_DUMMY: &str = "https://www.foo.de/my-area?token=base64jwt";
-    const SESS_ID_DUMMY: &str = "PHPSESSID-12345";
+    const SESS_ID_DUMMY: &str = "PHPSESSID123DUMMY";
     const EGYM_LOGIN_ERR_DUMMY: &str = "Egym login test-failure";
     const FF_LOGIN_ERR_DUMMY: &str = "FF login test-failure";
 
@@ -84,7 +90,7 @@ mod test {
             Some(|| Err(Box::from("FF-login not tested here")))
         );
 
-        let mut login_service: LoginService<HttpClientMock> = Default::default();
+        let mut login_service: LoginService<HttpClientMock, CookieMock> = Default::default();
         let req = EgymLoginRequest::new("user", "password", "client-id");
         let success = login_service.do_login(req).await;
 
@@ -99,7 +105,7 @@ mod test {
             Some(|| Err(Box::from(EGYM_LOGIN_ERR_DUMMY))),
             MockCall::None
         );
-        let mut login_service: LoginService<HttpClientMock> = Default::default();
+        let mut login_service: LoginService<HttpClientMock, CookieMock> = Default::default();
         let req = EgymLoginRequest::new("user", "password", "client-id");
         let success = login_service.do_login(req).await;
 
@@ -113,7 +119,7 @@ mod test {
             Some(|| egym_login_response_dummy(EGYM_JWT_DUMMY)),
             Some(|| ff_login_response_dummy(SESS_ID_DUMMY))
         );
-        let mut login_service: LoginService<HttpClientMock> = Default::default();
+        let mut login_service: LoginService<HttpClientMock, CookieMock> = Default::default();
         let req = EgymLoginRequest::new("user", "password", "client-id");
         let success = login_service.do_login(req).await;
 
@@ -129,13 +135,13 @@ mod test {
             Some(|| egym_login_response_dummy(EGYM_JWT_DUMMY)),
             Some(|| Err(Box::from(FF_LOGIN_ERR_DUMMY)))
         );
-        let mut login_service: LoginService<HttpClientMock> = Default::default();
+        let mut login_service: LoginService<HttpClientMock, CookieMock> = Default::default();
         let req = EgymLoginRequest::new("user", "password", "client-id");
         let success = login_service.do_login(req).await;
 
         assert!(success.is_err());
         assert!(login_service.token.is_some());
-        assert_eq!(EGYM_JWT_DUMMY, login_service.token.unwrap());
+        assert_eq!("base64jwt", login_service.token.unwrap());
         assert!(login_service.session.is_none());
     }
 }
