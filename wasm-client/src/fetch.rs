b58d190ddb9c872d21275513ccda12aff9b866ee
niilz <niilz@outlook.de>
2024-06-15 16:39:10 +0200

    Abstract over Window and impl test for client

--- wasm-client/src/fetch.rs
+++ wasm-client/src/fetch.rs
@@ -1,21 +1,42 @@
 use wasm_bindgen::{JsCast, JsValue};
 use wasm_bindgen_futures::JsFuture;
-use web_sys::{RequestInit, Response};
+use wasm_bindgen_test::console_log;
+use web_sys::{js_sys, RequestInit, Response, Window as WebSysWindow};
+
+pub(crate) trait Window {
+    fn instance() -> Option<impl Window>;
+    fn fetch(&self, url: &str, init: &RequestInit) -> js_sys::Promise;
+}
+
+impl Window for WebSysWindow {
+    fn instance() -> Option<impl Window> {
+        web_sys::window()
+    }
+    fn fetch(&self, url: &str, init: &RequestInit) -> js_sys::Promise {
+        self.fetch_with_str_and_init(url, init)
+    }
+}
 
 pub(crate) async fn client(
     method: &str,
     url: &str,
     body: Option<&str>,
+    window: &impl Window,
 ) -> Result<JsValue, JsValue> {
-    let window = web_sys::window().ok_or("Window unavailable")?;
     let mut init = RequestInit::new();
     init.body(Some(&JsValue::from(body)));
     init.method(method);
 
-    let promise = window.fetch_with_str_and_init(url, &init);
-    let res = JsFuture::from(promise).await?.dyn_into::<Response>()?;
-
-    JsFuture::from(res.json()?).await
+    let promise = window.fetch(url, &init);
+    let future = JsFuture::from(promise).await?;
+    //console_log!("future: {future:?}");
+    let res = future.dyn_into::<Response>()?;
+    //console_log!("res: {res:?}");
+    let json = res.json()?;
+    //console_log!("json: {json:?}");
+    let json_future = JsFuture::from(json).await;
+    //console_log!("future: {future:?}");
+    json_future
 }
 
 // TODO: make configurable
@@ -31,5 +52,53 @@ pub(crate) fn lambda_url(func: &str, session: Option<&str>) -> String {
 
 #[cfg(test)]
 mod tests {
-    // TODO:
+    use wasm_bindgen::JsValue;
+    use wasm_bindgen_test::wasm_bindgen_test;
+    use web_sys::{
+        js_sys::{self, JSON},
+        RequestInit, Response,
+    };
+
+    use crate::fetch::client;
+
+    use super::{lambda_url, Window};
+
+    #[test]
+    fn url_with_session() {
+        let session_id_dummy = "12345";
+        let lambda_fn = "frobnify";
+        let url = lambda_url(lambda_fn, Some(session_id_dummy));
+        assert_eq!(
+            url,
+            "http://localhost:9000/lambda-url/frobnify?session=12345"
+        );
+    }
+
+    struct MockWindow;
+
+    const JSON_DUMMY: &str = r#"{"key":"value"}"#;
+
+    impl Window for MockWindow {
+        fn instance() -> Option<impl Window> {
+            Some(MockWindow)
+        }
+        fn fetch(&self, _url: &str, _init: &RequestInit) -> js_sys::Promise {
+            let response = Response::new_with_opt_str(Some(JSON_DUMMY)).unwrap();
+            js_sys::Promise::resolve(&JsValue::from(&response))
+        }
+    }
+
+    #[wasm_bindgen_test]
+    async fn login_by_user_and_pasword() {
+        let window = MockWindow::instance().unwrap();
+
+        let json_res = client("POST", "foo", Some("body"), &window).await;
+
+        assert!(json_res.is_ok());
+
+        assert_eq!(
+            JSON::stringify(&json_res.unwrap()).unwrap().to_string(),
+            JSON_DUMMY
+        );
+    }
 }
--- wasm-client/src/lib.rs
+++ wasm-client/src/lib.rs
@@ -1,6 +1,8 @@
+use fetch::Window;
 use shared::dto::login_data::LoginData;
 use wasm_bindgen::prelude::wasm_bindgen;
 use wasm_bindgen::JsValue;
+use web_sys::Window as WebSysWindow;
 
 mod fetch;
 
@@ -13,14 +15,18 @@ pub async fn login(user_name: String, password: String) -> Result<JsValue, JsVal
     let login_url = fetch::lambda_url("login-lambda", None);
     let login_data = serde_json::to_string(&login_data).expect("login_data to Json");
 
-    fetch::client("POST", &login_url, Some(&login_data)).await
+    let window = WebSysWindow::instance().ok_or("Window unavailable")?;
+
+    fetch::client("POST", &login_url, Some(&login_data), &window).await
 }
 
 #[wasm_bindgen]
 pub async fn fetch_courses(session_id: &str) -> Result<JsValue, JsValue> {
     let courses_url = fetch::lambda_url("courses-lambda", Some(session_id));
 
-    fetch::client("GET", &courses_url, None).await
+    let window = WebSysWindow::instance().ok_or("Window unavailable")?;
+
+    fetch::client("GET", &courses_url, None, &window).await
 }
 
 #[cfg(test)]
