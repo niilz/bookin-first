64b4ccec713176c4ee63bce7fd06f61a27e64b9f
niilz <niilz@outlook.de>
2024-06-30 12:51:38 +0200

    Setup java-script/html api for login and display courses in app-mode

--- js/app.js
+++ js/app.js
@@ -6,6 +6,7 @@ import init, {
 import { displayCourses, displaySlots } from "./display.js";
 
 const USER_CREDENTIALS = "userCredentials";
+const USER_CREDENTIALS_APP_MODE = "userCredentialsAppMode";
 
 async function initWasm() {
   console.log("> init");
@@ -31,6 +32,7 @@ initWasm()
   .catch((e) => console.error(`init failed ${e}`));
 
 let userCredentials;
+let userCredentialsAppMode;
 
 // Login-Inputs
 const loginFormEl = document.querySelector("#login-form");
@@ -41,10 +43,21 @@ const selectListEl = document.querySelector("#select-list");
 
 async function tryLoadCourses() {
   const userCredentialsString = localStorage.getItem(USER_CREDENTIALS);
+  const userCredentialsAppModeString = localStorage.getItem(
+    USER_CREDENTIALS_APP_MODE
+  );
   if (userCredentialsString) {
     userCredentials = JSON.parse(userCredentialsString);
     console.log({ userCredentials });
     return await loadAndDisplayCourses(userCredentials.session, selectListEl);
+  } else if (userCredentialsAppModeString) {
+    userCredentialsAppMode = JSON.parse(userCredentialsAppMode);
+    console.log({ userCredentialsAppMode });
+    return await loadAndDisplayCourses(
+      userCredentials.session,
+      selectListEl,
+      true
+    );
   } else if (loginFormEl.classList.contains("hidden")) {
     // Show login-form if credentials are not present
     loginFormEl.classList.remove("hidden");
@@ -63,6 +76,7 @@ selectListEl.addEventListener("click", async (e) => {
 });
 
 const coursesButton = document.querySelector("#courses-button");
+const coursesButtonAppMode = document.querySelector("#courses-button-app-mode");
 coursesButton.addEventListener("click", async (e) => {
   e.preventDefault();
   if (!userCredentials) {
@@ -74,19 +88,43 @@ coursesButton.addEventListener("click", async (e) => {
   loadAndDisplayCourses(session);
 });
 
-async function login(username, password) {
-  userCredentials = await loginWasm(username, password);
-  localStorage.setItem(USER_CREDENTIALS, JSON.stringify(userCredentials));
-
-  console.log({ userCredentials });
+coursesButtonAppMode.addEventListener("click", async (e) => {
+  e.preventDefault();
+  if (!userCredentialsAppMode) {
+    const username = usernameInputEl.value;
+    const password = passwordInputEl.value;
+    login(username, password, true);
+  }
+  const { session } = userCredentials;
+  loadAndDisplayCourses(session, true);
+});
+async function login(username, password, isAppMode) {
+  if (isAppMode) {
+    userCredentialsAppMode = await loginWasmAppMode(username, password);
+    localStorage.setItem(
+      USER_CREDENTIALS_APP_MODE,
+      JSON.stringify(userCredentialsAppMode)
+    );
+    console.log({ userCredentialsAppMode });
+  } else {
+    userCredentials = await loginWasm(username, password);
+    localStorage.setItem(USER_CREDENTIALS, JSON.stringify(userCredentials));
+    console.log({ userCredentials });
+  }
 }
 
-async function loadAndDisplayCourses(sessionId) {
-  const courseResult = await coursesWasm(sessionId);
+async function loadAndDisplayCourses(sessionId, isAppMode) {
+  let courseResult;
+  if (isAppMode) {
+    courseResult = await coursesWasmAppMode(sessionId);
+  } else {
+    courseResult = await coursesWasm(sessionId);
+  }
   displayCourses(courseResult, selectListEl);
   return courseResult;
 }
 
+// Only relevant for non web mode (not app mode)
 async function loadAndDisplaySlots(sessionId, courseId, selectListEl) {
   const slots = await slotsWasm(sessionId, courseId);
   displaySlots(slots, selectListEl);
