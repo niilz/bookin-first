06d02d4a15f4f6f4d3dff322f065f7cf47d70477
niilz <niilz@outlook.de>
2024-07-06 14:26:54 +0200

    Add a netpulse-login-response

--- shared/src/dto/response.rs
+++ shared/src/dto/response.rs
@@ -7,6 +7,7 @@ pub enum Response {
     Text(String),
     Session(String),
     Json(String),
+    WithSession { response: String, session: String },
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq)]
@@ -28,4 +29,42 @@ pub struct BookingResponse {
     pub course_id: usize,
     #[serde(rename = "customerId")]
     pub customer_id: usize,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, Default)]
+#[wasm_bindgen(getter_with_clone)]
+pub struct NetpulseLoginResponse {
+    #[serde(rename = "uuid")]
+    pub user_id: String,
+    #[serde(rename = "firstName")]
+    pub first_name: String,
+    #[serde(rename = "lastName")]
+    pub last_name: String,
+    pub verified: bool,
+    #[serde(rename = "emailVerified")]
+    pub email_varified: bool,
+    #[serde(rename = "homeClubUuid")]
+    pub club_id: String,
+    #[serde(rename = "homeClubName")]
+    pub club_name: String,
+    #[serde(rename = "chainUuid")]
+    pub chain_uuid: String,
+    #[serde(rename = "chainName")]
+    pub chain_name: String,
+    pub timezone: String,
+    #[serde(rename = "timezoneOffset")]
+    pub timezone_offset: String,
+    #[serde(rename = "profileCompleted")]
+    pub profile_completed: bool,
+    #[serde(rename = "membershipType")]
+    pub membership_type: String,
+    //"barcode": null,
+    //"externalAuthToken": null,
+    //"measurementUnit": "M",
+    //"hasMessages": null,
+    //"guestPassUser": false,
+    //"externalRefreshToken": null,
+    //"email": null,
+    //"customInfo": null,
+    //"newUserSecret": null
 }
