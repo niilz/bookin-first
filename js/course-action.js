bddb2fe9dfdebc32fadb4145b1a791945b5c526d
niilz <niilz@outlook.de>
2024-07-21 13:24:13 +0200

    Cache course result for a day

--- js/course-action.js
+++ js/course-action.js
@@ -5,13 +5,30 @@ import {
 } from "../wasm-client/pkg/wasm_client.js";
 import { fetchUserCredentials } from "./login.js";
 
+const COURSES_KEY = "courses";
+const ONE_DAY = 1000 * 60 * 60 * 24;
+
 const slotListEl = document.querySelector("#select-list");
 
-slotListEl.addEventListener("click", bookCourseSlot);
+slotListEl.addEventListener("click", bookOrCancelCourseSlot);
 
 export async function loadCourses(userCredentials) {
   if (userCredentials.mode === "app") {
-    return coursesWasm(userCredentials["session"], userCredentials["user_id"]);
+    const cachedCourses = getCourses();
+    if (cachedCourses) {
+      const { lastStored, courses } = cachedCourses;
+      const sinceLastStored = Date.now() - lastStored;
+      if (courses && sinceLastStored < ONE_DAY) {
+        console.log("returning cached courses");
+        return courses;
+      }
+    }
+    const freshCourses = await coursesWasm(
+      userCredentials["session"],
+      userCredentials["user_id"]
+    );
+    storeCourses(freshCourses);
+    return freshCourses;
   } else if (userCredentials.mode === "web") {
     return coursesWasm(userCredentials.session, "");
   } else {
@@ -35,4 +52,17 @@ export async function bookCourseSlot(event) {
     const booking = await bookCourseSlotWasm(bookingRequest, session, userId);
     console.log({ booking });
   }
+}
+
+function storeCourses(courses) {
+  const lastStored = Date.now();
+  console.log("Storing courses");
+  const coursesData = JSON.stringify({ lastStored, courses });
+  window.localStorage.setItem(COURSES_KEY, coursesData);
+}
+
+function getCourses() {
+  const coursesData = window.localStorage.getItem(COURSES_KEY);
+  console.log("Loading courses");
+  return JSON.parse(coursesData);
 }
