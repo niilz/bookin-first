1f88a609defd87fef9041511f4aa4774859735e1
niilz <niilz@outlook.de>
2024-07-24 23:17:24 +0200

    fix render after book/cancel

--- js/course-mapper.js
+++ js/course-mapper.js
@@ -29,6 +29,7 @@ function mapSlot(course, slotId) {
     end,
     maxCapacity,
     totalBooked,
+    booked: false,
   };
   return slot;
 }
--- js/display.js
+++ js/display.js
@@ -1,5 +1,9 @@
-export function displayCourses(courseSlots, courseListEl, mode) {
+// Displayed Data
+const selectListEl = document.querySelector("#select-list");
+
+export function displayCourses(courseSlots, mode) {
   console.log("Displaying courses");
+  selectListEl.innerHTML = "";
   const courseListItems = courseSlots
     .entries()
     .map(([courseKey, slots], idx) => {
@@ -10,26 +14,28 @@ export function displayCourses(courseSlots, courseListEl, mode) {
         return `<li id="course-${idx}" class="course" data-course-id="${id}">
          <div>
             <p>${name}</p>
-            ${slotsHtml(slots)}
+            ${slotsHtml(name, slots)}
          </div>
        </li>`;
       }
     });
   for (const course of courseListItems) {
-    courseListEl.innerHTML += course;
+    selectListEl.innerHTML += course;
   }
 }
 
 // APP-Mode
-function slotsHtml(slots) {
+function slotsHtml(courseName, slots) {
   const html = slots
     .map(
-      ({ courseId, slotId, start, end, maxCapacity, totalBooked }, idx) =>
+      (
+        { courseId, slotId, start, end, maxCapacity, totalBooked, booked },
+        idx
+      ) =>
         `<p id="slot-${idx}"
-            class="slot"
-            data-course-id="${courseId}"
-            data-slot-id="${slotId}"
-            data-booked="false">
+            class="slot ${booked ? "booked" : ""}"
+            data-course-id="${courseId}:${courseName}"
+            data-slot-id="${slotId}">
                 Start: ${start}
                 <br>
                 End: ${end}
@@ -47,13 +53,13 @@ function slotsHtml(slots) {
 }
 
 // WEB-Mode
-export function displaySlotsWeb(slots, slotListEl) {
-  slotListEl.innerHTML = "";
+export function displaySlotsWeb(slots) {
+  selectListEl.innerHTML = "";
   const slotListItems = slots.map((slot, idx) => {
     let { id, start_date_time, booked_participants, max_participants } = slot;
     return `<li id="slot-${idx}" class="slot" data-slot-id="${id}">start: ${start_date_time}, participants: ${booked_participants}, max: ${max_participants}</li>`;
   });
   for (const slot of slotListItems) {
-    slotListEl.innerHTML += slot;
+    selectListEl.innerHTML += slot;
   }
 }
